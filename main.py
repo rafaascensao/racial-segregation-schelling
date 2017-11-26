92c33f6ec59afd16afdd4e0a5ccccde0aad712cc
Joao Rodrigues <joaovalentimrodrigues95@gmail.com>
2017-11-26 00:36:38 +0000

    same but with scale free graphs. not finished, does not converge even whith threshold = 0

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from matrix import  Matrix
+from scalefree import Scale_Free
 import matplotlib.pyplot as plt
 
-def main():
+def run_matrix():
     m = Matrix()
     print(m.matrix)
     unsatisfied = m.assert_unsatisfied()
@@ -19,5 +20,18 @@ def main():
     plt.imshow(m.matrix, interpolation='nearest')
     plt.show()
 
+def run_graph():
+    g = Scale_Free()
+    unsatisfied = g.assert_unsatisfied()
+    print("unsatisfied:", unsatisfied)
+    steps = 0
+    while len(unsatisfied) > 0:
+        g.move_unsatisfied(unsatisfied)
+        unsatisfied = g.assert_unsatisfied()
+        print("Unsatisfied percentage = ", (len(unsatisfied) / g.size) * 100)
+        steps += 1
+        print("unsatisfied:", unsatisfied)
+    print("all satisfied.\n steps: ", steps)
+
 if __name__ == '__main__':
-    main()
+    run_matrix()
---
+++ scalefree.py
@@ -0,0 +1,68 @@
+import networkx as nx
+from random import choice
+
+class Scale_Free:
+    def __init__(self, size = 100, p_one = 0.3, p_two = 0.3, threshold = 0.0):
+
+        self.size = size
+        self.n_one = int(self.size * p_one)
+        self.n_two = int(self.size * p_two)
+        self.n_empty = self.size - (self.n_one + self.n_two)
+        self.threshold = threshold
+
+        self.graph = nx.scale_free_graph(size)
+        self.races = {node: 0 for node in self.graph.nodes()}
+        self.populate()
+
+    def populate(self):
+        empty_positions = self.empty_nodes()
+
+        for i in range(self.n_one):
+            node = choice(empty_positions)
+            self.races[node] = 1
+            empty_positions.remove(node)
+
+        for i in range(self.n_one):
+            node = choice(empty_positions)
+            self.races[node] = 2
+            empty_positions.remove(node)
+
+    def empty_nodes(self) -> list :
+        empty = [node for node in self.races.keys() if self.races[node] == 0]
+        return empty
+
+    def assert_unsatisfied(self):
+        unsatisfied = []
+        for node in self.graph.nodes():
+            if self.races[node] == 0:
+                continue
+            elif self.check_neighborhood(node):
+                unsatisfied.append(node)
+        return unsatisfied
+
+    def check_neighborhood(self, node):
+        neighbors = self.graph.neighbors(node)
+        my_race = self.races[node]
+        same_race = 0
+        num_neighbors = 0
+        for n in neighbors:
+            if self.races[n] == 0:
+                continue
+            elif self.races[n] == my_race:
+                same_race += 1
+            num_neighbors += 1
+
+        try:
+            ratio = same_race / num_neighbors
+        except ZeroDivisionError:
+            return True
+
+        return ratio > self.threshold
+
+    def move_unsatisfied(self, unsat):
+        empty_positions = self.empty_nodes()
+
+        for u in unsat:
+            random_empty = choice(empty_positions)
+            self.races[random_empty] = self.races[u]
+            self.races[u] = 0
