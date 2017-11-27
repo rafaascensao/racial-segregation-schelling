396739f039763ae52ccaa60d6adbefba7d1d98ad
JoaoMVR <joaovalentimrodrigues95@gmail.com>
2017-11-27 10:29:42 +0000

    polotting the unsatisfiability over steps

--- main.py
+++ main.py
@@ -5,18 +5,25 @@ def main():
     m = Matrix()
     print(m.matrix)
     unsatisfied = m.assert_unsatisfied()
-
+    unsat_evolution = []
     print("Unsatisfied percentage = ", (len(unsatisfied) / m.entries) * 100)
     steps = 0
     while len(unsatisfied) > 0:
         m.move_unsatisfied(unsatisfied)
         #print(m.matrix)
         unsatisfied = m.assert_unsatisfied()
-        print("Unsatisfied percentage = ", (len(unsatisfied) / m.entries) * 100)
+        unsat_evolution.append(len(unsatisfied) / m.entries * 100)
         steps += 1
 
+
     print("all satisfied.\n steps: ", steps)
+
+    step_list = [i for i in range(steps)]
+
     plt.imshow(m.matrix, interpolation='nearest')
+    plt.show()
+
+    plt.scatter(step_list, unsat_evolution)
     plt.show()
 
 if __name__ == '__main__':
